4225c25a3deda74c1d91979ef6f27232ae7ef405
Luke Holt <luke.holt55@gmail.com>
2022-10-25 23:58:10 -0300

    fixed spi write_read function
    clang-format

--- include/event_msg.h
+++ include/event_msg.h
@@ -1,51 +1,47 @@
+#ifndef EVENT_MSG_H__
+#define EVENT_MSG_H__
+
 #include <stdint.h>
 
-
 typedef struct event_msg_t {
-
-	uint8_t grpno;
-	uint8_t eventno;
-	void *ctx;
+  uint8_t grpno;
+  uint8_t eventno;
+  void *ctx;
 
 } event_msg_t;
 
-
 typedef void (*event_handler_t)(event_msg_t *msg);
-
 
 /**
  * @brief Register an event handler in exchange for the group number.
  * When sending adding an event to the queue, this group number needs to
  * be included to specify which handler to call.
- * 
+ *
  * @return int Group number
  */
 int event_register_group(event_handler_t handler);
-
-
 
 /**
  * @brief Function to process message from queue.
  * Use this function to manually process messages.
  * The event loop calls this function whenever there are messages in queue.
- * 
+ *
  */
 void process_msg(void);
 
-
-
 /**
  * @brief Begin the event loop.
- * 
+ *
  */
 void event_begin_loop(void);
 
-
 /**
  * @brief Add message to queue
- * 
+ *
  * @param msg Pointer to message structure
  * @return int errno
  */
 int event_enqueue(event_msg_t *msg);
 /* TODO: EVENT: Maybe add priority system? */
+
+#endif
--- include/lcd1602a.h
+++ include/lcd1602a.h
@@ -1,7 +1,11 @@
-#define LCD_TOP_ROW		0
-#define LCD_BOT_ROW		1
+#ifndef LCD1602A_H__
+#define LCD1602A_H__
 
+#define LCD_TOP_ROW 0
+#define LCD_BOT_ROW 1
 
 void lcd_init(void);
 void lcd_println(char *str, uint8_t row);
 void lcd_shift_down(void);
+
+#endif
